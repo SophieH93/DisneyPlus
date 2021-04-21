12819fc2e4a9106ca8364dc61e2672bf75db20b9
Sophie <sophiehickey123@gmail.com>
2021-04-21 22:29:09 +0100

    created nav link wit img and styled this

--- disney-plus/src/components/Header.js
+++ disney-plus/src/components/Header.js
@@ -10,7 +10,12 @@ function Header() {
         "
         />
       </Logo>
-      <NavMenue>Menue</NavMenue>
+      <NavMenue>
+        <a href="/home">
+          <img src="/images/home-icon.svg" />
+          <span>HOME</span>
+        </a>
+      </NavMenue>
     </Nav>
   );
 }
@@ -52,9 +57,58 @@ const NavMenue = styled.div`
   position: relative;
   margin: 0 auto 0 25px;
 
-  @media (max-width: 768px) {
-    display: none;
+  a {
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
+    text-decoration: none;
+
+    img {
+      height: 20px;
+      min-width: 20px;
+      width: 20px;
+      z-index: auto;
+    }
+
+    span {
+      color: rgb(249, 249, 249);
+      font-size: 13px;
+      letter-spacing: 1.42px;
+      line-height: 1.08;
+      padding: 2px 0px;
+      white-space: nowrap;
+      position: relative;
+
+      &:before {
+        background-color: rgb(249, 249, 249);
+        border-radius: 0px 0px 4px 4px;
+        bottom: -6px;
+        content: "";
+        height: 2px;
+        left: 0;
+        opacity: 0;
+        position: absolute;
+        right: 0px;
+        transform-origin: left center;
+        transform: scaleX(0);
+        transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+        visibility: hidden;
+        width: auto;
+      }
+    }
+
+    &:hover {
+      span:before {
+        transform: scaleX(1);
+        visibility: visible;
+        opacity: 1 !important;
+      }
+    }
   }
+
+  //   @media (max-width: 768px) {
+  //     display: none;
+  //   }
 `;
 
 export default Header;
