cd9d7a0053c5d8f11eafc85b0f08efe09cae3d2c
Sophie <sophiehickey123@gmail.com>
2021-04-22 22:29:35 +0100

    installed react-slick & carousel

--- disney-plus/src/components/Home.js
+++ disney-plus/src/components/Home.js
@@ -1,12 +1,10 @@
 import styled from "styled-components";
+import ImgSlider from "./ImgSlider";
 
 function Home() {
   return (
     <Container>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
-      <h2>hello</h2>
+      <ImgSlider />
     </Container>
   );
 }
